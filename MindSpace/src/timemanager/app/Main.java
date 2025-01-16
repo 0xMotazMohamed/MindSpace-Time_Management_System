899fef412c8f479bc74fba2666f86776eb1b93c4
Abdullah <abdullahmostafa9098@gmail.com>
2025-01-16 15:45:08 +0200

    Account Sync Cloud

--- MindSpace/src/timemanager/app/Main.java
+++ MindSpace/src/timemanager/app/Main.java
@@ -4,6 +4,7 @@ import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import timemanager.data.dao.DataImpl;
 import timemanager.data.dao.GitHubDownloader;
 import timemanager.data.dao.GitHubUploader;
+import timemanager.data.dto.Account;
 import timemanager.gui.MainFrame;
 
 import javax.swing.*;
@@ -16,7 +17,12 @@ public class Main {
 
         //Display accounts (test)
         System.out.println("accounts:");
-//        DataImpl.getAccounts().forEach((username, account));
+        DataImpl.getAccounts().forEach((email, account) -> {
+            System.out.println("Name: " + account.getName());
+            System.out.println("Email: " + account.getEmail());
+            System.out.println("Password: " + account.getPassword());
+            System.out.println("Projects: " + account.getProjects());
+        });
 
         //Save when shutdown
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
